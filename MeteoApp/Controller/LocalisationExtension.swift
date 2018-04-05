82ada095c95119956dc6103d92574ecee6b74bc2
Dea-loC <dealoc@gmail.com>
2018-04-05 18:21:21 +0200

    Ajout des pods + Alamofire

--- MeteoApp/Controller/LocalisationExtension.swift
+++ MeteoApp/Controller/LocalisationExtension.swift
@@ -26,7 +26,7 @@ extension MeteoController: CLLocationManagerDelegate {
         let positionActuelle = locations[0]
         let latitude = positionActuelle.coordinate.latitude
         let longitude = positionActuelle.coordinate.longitude
-        print("Latitude: \(latitude) -> longitude: \(longitude)")
+        obtenirPrevisionsMeteo(latitude: latitude, longitude: longitude)
         
     }
     
