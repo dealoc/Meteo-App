a643859a910704b69d35a928828cf4e4df2700ba
Dea-loC <dealoc@gmail.com>
2018-04-05 16:37:13 +0200

    Ajout du gitignore

---
+++ MeteoApp/Ressources/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  MeteoApp
+//
+//  Created by Dea-loC on 05/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import Foundation
+
+let API = "a9e8c817b5484a1d86bbcb9dadf802dd"
+
